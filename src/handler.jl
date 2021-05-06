552630c55b015c52da98dd55da112037ea226257
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-05-06 11:47:44 +0100

    do not use disjoint as incompatible with 1.0

--- src/handler.jl
+++ src/handler.jl
@@ -23,7 +23,8 @@ Names with a '.' separators will be used to form subdirectories
 (e.g., "Foo.bar.x" will be saved to "\$prefix/Foo/bar/x.jlso").
 """
 function path(handler::Handler{P}, name::String; tags...) where P
-    isdisjoint(TAGS[], tags) || ArgumentError("application and package tags can not be the same TODO: rephrase")
+    joint_tags = intersect(TAGS[], tags)
+    isempty(joint_tags) || ArgumentError("Application- and package-level tags both contain $(joint_tags).")
     all_tags = collect(Iterators.flatten((TAGS[], tags)))
 
     # Build up a path prefix based on the tags passed in.
