8a4c8d8d87a69e582d7b87eca808b427843ec2c2
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-05-07 14:17:50 +0100

    add docs

--- src/Checkpoints.jl
+++ src/Checkpoints.jl
@@ -39,12 +39,11 @@ For example
 ```julia
 with_tags(:foo=>1, :bar=>2) do
     q_out = qux()
-    checkpoint("foobar", :output=q_out)
+    checkpoint("foobar"; :output=q_out)
 end
 ```
-This snippted will result in   `"foobar"` checkpoint having the `foo=1` and `bar=2` tags, as will any checkpoints created by `qux`().
-The context tags are[dynamically scoped](https://en.wikipedia.org/wiki/Scope_(computer_science)#Lexical_scope_vs._dynamic_scope_2) and so are remembered through function calls.
-[`checkpoint`](@ref) call.
+This snippted will result in `"foobar"` checkpoint having the `foo=1` and `bar=2` tags, as will any checkpoints created by `qux`().
+The context tags are [dynamically scoped](https://en.wikipedia.org/wiki/Scope_(computer_science)#Lexical_scope_vs._dynamic_scope_2) and so are remembered through function calls.
 
 Nested contexts (nested `with_tags` calls) are allowed. Duplicate tag names and values are
 allowed, including the tags provided directly in the [`checkpoint`](@ref) call.
