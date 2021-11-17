315cf21a6369d88c6ce4a65d3f40a14efc718ee9
Lyndon White <oxinabox@ucc.asn.au>
2021-11-17 17:26:41 +0000

    Apply suggestions from code review
    Co-authored-by: Ben Cottier <bcottier2@gmail.com>

--- src/indexing.jl
+++ src/indexing.jl
@@ -67,9 +67,9 @@ prefixes(x::IndexEntry) = getfield(x, :prefixes)
 """
     checkpoint_fullname(x::IndexEntry)
 
-The fullname of the checkpoint output file, including [`prefixes`](@ref), and 
+The full name of the checkpoint output file, including [`prefixes`](@ref), and 
 [`checkpoint_name`](@ref).
-If the checkpoint was saved used `checkpoint(Forecasters, "forecasts", ...)` then
+If the checkpoint was saved using `checkpoint(Forecasters, "forecasts", ...)` then
 `checkpoint_fullname` will return `"Forecasters.forecasts"`.
 If the checkpoint was saved using `checkpoint("Foo.Bar", "forecasts.jlso")` then
 `checkpoint_fullname` will return `"Foo.Bar.forecasts"`.
