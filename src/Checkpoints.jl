81d30273a27f2bcf01fc0fd1dcd1d7f8fa641acc
Rory Finnegan <rory.finnegan@invenia.ca>
2022-07-19 13:45:57 -0700

    Update `enabled_checkpoints` docstring.

--- src/Checkpoints.jl
+++ src/Checkpoints.jl
@@ -72,7 +72,8 @@ available() = collect(keys(CHECKPOINTS))
 """
     enabled_checkpoints() -> Vector{String}
 
-Returns a vector of all enabled ([`config`](@ref)ured) checkpoints.
+Returns a vector of all enabled ([`config`](@ref)ured) and not [`deprecate`](@ref)d checkpoints.
+Use [`deprecated_checkpoints`](@ref) to retrieve a mapping of old / deprecated checkpoints.
 """
 enabled_checkpoints() = filter(k -> CHECKPOINTS[k] isa Handler, available())
 
