c6d43fb031c7657ed3c5e29281a6af2e5307b79e
Rory Finnegan <rory.finnegan@invenia.ca>
2023-02-14 12:19:25 -0800

    Introduce a Handler hierarchy and DictHandler.

--- src/handler.jl
+++ src/handler.jl
@@ -1,18 +1,68 @@
-struct Handler{P<:AbstractPath}
+abstract type AbstractHandler end
+
+"""
+    getkey(handler, name, separator="/") -> String
+
+Combine the `CONTEXT_TAGS` and `name` into a unique checkpoint key as a string.
+If the checkpoint name includes `.`, usually representing nested modules, these are
+also replaced with the provided separator.
+"""
+function getkey(::AbstractHandler, name::String, separator="/")::String
+    prefix = ["$key=$val" for (key, val) in CONTEXT_TAGS[]]
+    parts = split(name, '.')  # Split up the name by '.'
+    return Base.join(vcat(prefix, parts), separator)
+end
+
+path(args...) = Path(getkey(args...))
+
+"""
+    stage!(handler::AbstractHandler, objects, data::Dict{Symbol})
+
+Update the objects with the new data.
+By default all handlers assume objects implements the associative interface.
+"""
+function stage!(handler::AbstractHandler, objects, data::Dict{Symbol})
+    for (k, v) in data
+        objects[k] = v
+    end
+
+    return objects
+end
+
+"""
+    commit!(handler, prefix, objects)
+
+Serialize and write objects to a given path/prefix/key as defined by the handler.
+"""
+commit!
+
+#=
+Define our no-op conditions just to be safe
+=#
+function checkpoint(handler::Nothing, name::String, data::Dict{Symbol}; tags...)
+    checkpoint_deprecation(tags...)
+    with_checkpoint_tags(tags...) do
+        debug(LOGGER, "Checkpoint $name triggered, but no handler has been set.")
+        nothing
+    end
+end
+
+
+struct JLSOHandler{P<:AbstractPath} <: AbstractHandler
     path::P
     settings        # Could be Vector or Pairs on 0.6 or 1.0 respectively
 end
 
 """
-    Handler(path::Union{String, AbstractPath}; kwargs...)
-    Handler(bucket::String, prefix::String; kwargs...)
+    JLSOHandler(path::Union{String, AbstractPath}; kwargs...)
+    JLSOHandler(bucket::String, prefix::String; kwargs...)
 
 Handles iteratively saving JLSO file to the specified path location.
 FilePath are used to abstract away differences between paths on S3 or locally.
 """
-Handler(path::AbstractPath; kwargs...) = Handler(path, kwargs)
-Handler(path::String; kwargs...) = Handler(Path(path), kwargs)
-Handler(bucket::String, prefix::String; kwargs...) = Handler(S3Path("s3://$bucket/$prefix"), kwargs)
+JLSOHandler(path::AbstractPath; kwargs...) = JLSOHandler(path, kwargs)
+JLSOHandler(path::String; kwargs...) = JLSOHandler(Path(path), kwargs)
+JLSOHandler(bucket::String, prefix::String; kwargs...) = JLSOHandler(S3Path("s3://$bucket/$prefix"), kwargs)
 
 """
     path(handler, name)
@@ -22,35 +72,11 @@ Tags are used to dynamically prefix the named file with the handler's path.
 Names with a '.' separators will be used to form subdirectories
 (e.g., "Foo.bar.x" will be saved to "\$prefix/Foo/bar/x.jlso").
 """
-function path(handler::Handler{P}, name::String) where P
-    prefix = ["$key=$val" for (key,val) in CONTEXT_TAGS[]]
-
-    # Split up the name by '.' and add the jlso extension
-    parts = split(name, '.')
-    parts[end] = string(parts[end], ".jlso")
-
-    return join(handler.path, prefix..., parts...)
+function path(handler::JLSOHandler{P}, name::String) where P
+    return join(handler.path, getkey(handler, name) * ".jlso")
 end
 
-"""
-    stage!(handler::Handler, jlso::JLSOFIle, data::Dict{Symbol})
-
-Update the JLSOFile with the new data.
-"""
-function stage!(handler::Handler, jlso::JLSO.JLSOFile, data::Dict{Symbol})
-    for (k, v) in data
-        jlso[k] = v
-    end
-
-    return jlso
-end
-
-"""
-    commit!(handler, path, jlso)
-
-Write the JLSOFile to the path as bytes.
-"""
-function commit!(handler::Handler{P}, path::P, jlso::JLSO.JLSOFile) where P <: AbstractPath
+function commit!(handler::JLSOHandler{P}, path::P, jlso::JLSO.JLSOFile) where P <: AbstractPath
     # NOTE: This is only necessary because FilePathsBase.FileBuffer needs to support
     # write(::FileBuffer, ::UInt8)
     # https://github.com/rofinn/FilePathsBase.jl/issues/45
@@ -61,7 +87,7 @@ function commit!(handler::Handler{P}, path::P, jlso::JLSO.JLSOFile) where P <: A
     write(path, bytes)
 end
 
-function checkpoint(handler::Handler, name::String, data::Dict{Symbol}; tags...)
+function checkpoint(handler::JLSOHandler, name::String, data::Dict{Symbol}; tags...)
     checkpoint_deprecation(tags...)
     with_checkpoint_tags(tags...) do
         debug(LOGGER, "Checkpoint $name triggered, with context: $(join(CONTEXT_TAGS[], ", ")).")
@@ -72,13 +98,24 @@ function checkpoint(handler::Handler, name::String, data::Dict{Symbol}; tags...)
     end
 end
 
-#=
-Define our no-op conditions just to be safe
-=#
-function checkpoint(handler::Nothing, name::String, data::Dict{Symbol}; tags...)
+"""
+    DictHandler(objects)
+
+Saves checkpointed objects into a dictionary where the keys are strings generated from
+the checkpoint tags and name.
+"""
+struct DictHandler <: AbstractHandler
+    objects::Dict{String, Dict}
+    DictHandler() = new(Dict{String, Dict}())
+end
+
+commit!(handler::DictHandler, k::AbstractString, data) = setindex!(handler.objects, data, k)
+
+function checkpoint(handler::DictHandler, name::String, data::Dict{Symbol}; tags...)
+    # TODO: Remove duplicate wrapper code
     checkpoint_deprecation(tags...)
     with_checkpoint_tags(tags...) do
-        debug(LOGGER, "Checkpoint $name triggered, but no handler has been set.")
-        nothing
+        debug(LOGGER, "Checkpoint $name triggered, with context: $(join(CONTEXT_TAGS[], ", ")).")
+        handler.objects[getkey(handler, name)] = data
     end
 end
--- src/session.jl
+++ src/session.jl
@@ -1,4 +1,4 @@
-struct Session{H<:Union{Nothing, Handler}}
+struct Session{H<:Union{Nothing, AbstractHandler}}
     name::String
     handler::H
     objects::DefaultDict
@@ -8,11 +8,7 @@ function Session(name::String)
      # Create our objects dictionary which defaults to returning
     # an empty JLSOFile
     handler = CHECKPOINTS[name]
-
-    objects = DefaultDict{AbstractPath, JLSO.JLSOFile}() do
-        JLSO.JLSOFile(Dict{Symbol, Vector{UInt8}}(); handler.settings...)
-    end
-
+    objects = session_objects(handler)
     Session{typeof(handler)}(name, handler, objects)
 end
 
@@ -34,29 +30,45 @@ function Session(f::Function, prefix::Union{Module, String}, names::Vector{Strin
     Session(f, map(n -> "$prefix.$n", names))
 end
 
+function session_objects(handler)
+    return DefaultDict{AbstractString, Dict}() do
+        Dict{Symbol, Any}()
+    end
+end
+
+function session_objects(handler::JLSOHandler)
+    return DefaultDict{AbstractPath, JLSO.JLSOFile}() do
+        JLSO.JLSOFile(Dict{Symbol, Vector{UInt8}}(); handler.settings...)
+    end
+end
+
 """
     commit!(session)
 
-Write all staged JLSOFiles to the respective paths.
+Write all staged objects to the respective keys.
 """
 function commit!(session::Session)
     # No-ops skip when handler is nothing
     session.handler === nothing && return nothing
 
-    for (p, jlso) in session.objects
-        commit!(session.handler, p, jlso)
+    for (k, v) in session.objects
+        commit!(session.handler, k, v)
     end
 end
 
 function checkpoint(session::Session, data::Dict{Symbol}; tags...)
     checkpoint_deprecation(tags...)
     with_checkpoint_tags(tags...) do
-        # No-ops skip when handler is nothing
-        session.handler === nothing && return nothing
+        handler = session.handler
+        name = session.name
+        K = keytype(session.objects)
 
-        p = path(session.handler, session.name)
-        jlso = session.objects[p]
-        session.objects[p] = stage!(session.handler, jlso, data)
+        # No-ops skip when handler is nothing
+        handler === nothing && return nothing
+
+        # Our handler may not always be storing data in filepaths
+        k = K <: AbstractPath ? path(handler, name) : getkey(handler, name)
+        session.objects[k] = stage!(handler, session.objects[k], data)
     end
 end
 
